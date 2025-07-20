8fb24979617170f23b510143642b8201a7ad3392
Alex-Endevour <alejugger@gmail.com>
2025-07-19 18:26:03 -0600

    jh

---
+++ python/estudiar.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = "133fa1a"   # pon tu key
+
+def year_rating_votes_boxoffice(title):
+    d = requests.get("https://www.omdbapi.com/", params={"apikey": API_KEY, "t": title}, timeout=10).json()
+    if d.get("Response") != "True":
+        return None
+    votes = None if d.get("imdbVotes") in (None, "N/A") else int(d["imdbVotes"].replace(",", ""))
+    box_raw = d.get("BoxOffice")
+    box_int = None if box_raw in (None, "N/A") else int(box_raw.replace("$","").replace(",",""))
+    return {
+        "Title": d.get("Title"),
+        "Year": d.get("Year"),
+        "imdbRating": d.get("imdbRating"),
+        "imdbVotes": votes,
+        "BoxOffice": box_int,
+    }
+
+print(year_rating_votes_boxoffice("The Matrix"))
+
