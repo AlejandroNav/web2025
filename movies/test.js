1530bca0a4248aa5cf18efe8514746399fe99a73
Alex-Endevour <alejugger@gmail.com>
2026-03-22 19:12:49 -0600

    cambison 1.0

---
+++ movies/test.js
@@ -0,0 +1,7 @@
+const colors = require("colors");
+
+console.log("it works in green".green);
+console.log("this is red".red);
+console.log("blue text".blue);
+console.log("rainbow test".rainbow);
+console.log("bold yellow".yellow.bold);
